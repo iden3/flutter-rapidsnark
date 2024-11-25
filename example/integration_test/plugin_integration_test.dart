400aef2f85fb5081e93a83011938e0143bc1ebef
Yaroslav Moria <5eeman@users.noreply.github.com>
2024-11-25 13:55:11 +0100

    Added integration tests.

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -1,28 +1,105 @@
-// This is a basic Flutter integration test.
-//
-// Since integration tests run in a full Flutter application, they can interact
-// with the host side of a plugin implementation, unlike Dart unit tests.
-//
-// For more information about Flutter integration tests, please see
-// https://docs.flutter.dev/cookbook/testing/integration/introduction
+import 'dart:io';
 
+import 'package:circom_witnesscalc/circom_witnesscalc.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:flutter_rapidsnark/flutter_rapidsnark.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+const zkeyPath = 'assets/authV2.zkey';
+const inputsPath = 'assets/authV2_inputs.json';
+const wcdPath = 'assets/authV2.wcd';
+const vkPath = 'assets/authV2_verification_key.json';
+
+void main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('groth16Verify test', (WidgetTester tester) async {
-    final Rapidsnark plugin = Rapidsnark();
-    final proofValid = await plugin.groth16Verify(
-      proof: '',
-      inputs: '',
-      verificationKey: '',
-    );
-    // The version string depends on the host platform running the test, so
-    // just assert that some non-empty string is returned.
-    expect(proofValid, false);
+  Uint8List zkey = Uint8List(0);
+  String inputs = '';
+  Uint8List witnessGraph = Uint8List(0);
+  String verificationKey = '';
+
+  Uint8List witness = Uint8List(0);
+
+  setUpAll(() async {
+    zkey = (await rootBundle.load(zkeyPath)).buffer.asUint8List();
+    inputs = await rootBundle.loadString(inputsPath);
+    witnessGraph = (await rootBundle.load(wcdPath)).buffer.asUint8List();
+    verificationKey = await rootBundle.loadString(vkPath);
+
+    witness = (await CircomWitnesscalc().calculateWitness(
+      inputs: inputs,
+      graphData: witnessGraph.buffer.asUint8List(),
+    ))!;
   });
+
+  testWidgets('Buffer prove and verify test', (tester) async {
+    final Rapidsnark plugin = Rapidsnark();
+    final proof = await plugin.groth16Prove(
+      zkey: zkey,
+      witness: witness,
+    );
+
+    expect(proof.proof, isNotEmpty);
+    expect(proof.publicSignals, isNotEmpty);
+
+    final verify = await plugin.groth16Verify(
+      proof: proof.proof,
+      inputs: proof.publicSignals,
+      verificationKey: verificationKey,
+    );
+
+    expect(verify, isTrue);
+  });
+
+  testWidgets('Path prove and verify test', (tester) async {
+    final zkeyPath = await _copyZkeyToTempDir(zkey);
+
+    final Rapidsnark plugin = Rapidsnark();
+    final proof = await plugin.groth16ProveWithZKeyFilePath(
+      zkeyPath: zkeyPath,
+      witness: witness,
+    );
+
+    expect(proof.proof, isNotEmpty);
+    expect(proof.publicSignals, isNotEmpty);
+
+    final verify = await plugin.groth16Verify(
+      proof: proof.proof,
+      inputs: proof.publicSignals,
+      verificationKey: verificationKey,
+    );
+
+    expect(verify, isTrue);
+  });
+
+  testWidgets("Test public buffer size calc from buffer", (tester) async {
+    final Rapidsnark plugin = Rapidsnark();
+    final publicSize = await plugin.groth16PublicSizeForZkeyBuf(zkey: zkey);
+
+    expect(publicSize, isNot(0));
+  });
+
+  testWidgets("Test public buffer size calc from file path", (tester) async {
+    final zkeyPath = await _copyZkeyToTempDir(zkey);
+
+    final Rapidsnark plugin = Rapidsnark();
+    final publicSize =
+        await plugin.groth16PublicSizeForZkeyFile(zkeyPath: zkeyPath);
+
+    expect(publicSize, isNot(0));
+  });
+}
+
+Future<String> _copyZkeyToTempDir(Uint8List zkey) async {
+  final tempDir = await getTemporaryDirectory();
+  final zkeyPath = '${tempDir.path}/authV2.zkey';
+  final file = File(zkeyPath);
+  if (file.existsSync()) {
+    await file.delete();
+  }
+  await file.writeAsBytes(zkey);
+  return zkeyPath;
 }
